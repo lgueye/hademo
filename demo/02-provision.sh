4a91f71175f74dc9fba2ff01704ec0f17215e596
louis gueye <louis.gueye@gmail.com>
2018-10-28 00:16:38 +0200

    [hademo]: introduce playbook requirements

--- demo/02-provision.sh
+++ demo/02-provision.sh
@@ -2,6 +2,6 @@
 
 cd ../ansible
 
-export ANSIBLE_TF_DIR=../terraform/digitalocean && time ansible-playbook --vault-id ~/.ansible_vault_pass.txt -i /etc/ansible/terraform.py sos.yml -e "target_env=staging rev=`git log -1 --pretty='%h'` security_mode=secure"
+export ANSIBLE_TF_DIR=../terraform/digitalocean && ansible-galaxy install -r requirements.yml && time ansible-playbook --vault-id ~/.ansible_vault_pass.txt -i /etc/ansible/terraform.py sos.yml -e "target_env=staging rev=`git log -1 --pretty='%h'` security_mode=secure"
 
 cd -
